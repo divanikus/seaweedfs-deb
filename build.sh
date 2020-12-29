1d0a73f1597429f361b4425cb6ae4f0fdcc52b96
divanikus <3438036+divanikus@users.noreply.github.com>
2020-12-29 14:26:20 +0300

    Version bump

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=seaweedfs
-VERSION=2.16
+VERSION=2.18
 
 curl -OL https://github.com/chrislusf/seaweedfs/releases/download/$VERSION/linux_amd64.tar.gz
 tar -zxvf linux_amd64.tar.gz
